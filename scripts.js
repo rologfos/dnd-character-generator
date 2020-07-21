e2f60b38454e186f81b4c65dc288a0f3e0c27735
rologfos <55161206+rologfos@users.noreply.github.com>
2020-07-20 23:03:05 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-const app = document.getElementById('root')
+const box_name = document.getElementById('name')
+const box_race = document.getElementById('race')
+const box_job = document.getElementById('job')
 
 //Call Api
 var request = new XMLHttpRequest()
@@ -8,13 +10,18 @@ request.onload = function () {
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
     //Data recieved
-    const errorMessage = document.createElement('text')
-    errorMessage.textContent = `It Worked`
-    app.appendChild(errorMessage)
+    var text = document.createElement('text')
+    text.textContent = 'Greg'
+    char_name.appendChild(text)
+    text.textContent = 'Human'
+    char_race.appendChild(text)
+    text.textContent = 'Fighter'
+    char_class.appendChild(text)
+
   } else {
     //Error
     const errorMessage = document.createElement('text')
-    errorMessage.textContent = `D&D 5e api could not be reached`
+    errorMessage.textContent = 'D&D 5e api could not be reached'
     app.appendChild(errorMessage)
   }
 }
